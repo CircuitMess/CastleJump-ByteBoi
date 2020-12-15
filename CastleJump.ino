68d2fd5c7c67ff3261b848e956919724e1d83c04
hrvojekolar <hrvojekolar1998@gmail.com>
2020-12-15 19:42:32 +0100

    added Highscore.begin()

--- CastleJump.ino
+++ CastleJump.ino
@@ -5,12 +5,14 @@
 #include <Loop/LoopManager.h>
 #include "src/CastleJump.h"
 #include "src/GameState.h"
+#include "src/Highscore/Highscore.h"
 
 CastleJump *castleJump;
 
 void setup(){
 	Serial.begin(115200);
 	Nibble.begin();
+	Highscore.begin();
 	castleJump=new CastleJump();
 	LoopManager::addListener(castleJump);
 	LoopManager::addListener(Input::getInstance());
