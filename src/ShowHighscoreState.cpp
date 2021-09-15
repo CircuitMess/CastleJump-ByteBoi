b0255ec001212d17013952c0f218c7ab05d886b5
hrvojekolar <hrvojekolar1998@gmail.com>
2021-09-15 09:09:45 +0200

    adapt for ByteBoi

--- src/ShowHighscoreState.cpp
+++ src/ShowHighscoreState.cpp
@@ -5,7 +5,6 @@
 #include "Highscore/Highscore.h"
 #include "Melodies/Notes.hpp"
 #include "bitmaps/highscore.hpp"
-#include "Pins.hpp"
 
 CastleJump::ShowHighscoreState *CastleJump::ShowHighscoreState::instance = nullptr;
 
@@ -31,21 +30,21 @@ void CastleJump::ShowHighscoreState::start(CastleJump &gameEnter){
 
 	castleJump=&gameEnter;
 	Input::getInstance()->setBtnPressCallback(BTN_UP,[](){
-		Piezo.tone(NOTE_B7,100);
+		//	Piezo.tone(NOTE_B7,100);
 		instance->castleJump->deleteHighscores();
 	});
 	Input::getInstance()->setBtnPressCallback(BTN_A,[](){
-		Piezo.tone(NOTE_B6,25);
+		//	Piezo.tone(NOTE_B6,25);
 		instance->castleJump->returnToMenu();
 	});
 	Input::getInstance()->setBtnPressCallback(BTN_B,[](){
-		Piezo.tone(NOTE_B6,25);
+		//	Piezo.tone(NOTE_B6,25);
 		instance->castleJump->returnToMenu();
 	});
 	Input::getInstance()->setBtnPressCallback(BTN_C, [](){
 		instance->castleJump->returnToMenu();
 	});
-	Piezo.setMute(true);
+//	Piezo.setMute(true);
 }
 void CastleJump::ShowHighscoreState::stop(){
 	Input::getInstance()->removeBtnPressCallback(BTN_A);
@@ -68,7 +67,7 @@ void CastleJump::ShowHighscoreState::drawHighscore(){
 		baseSprite->setTextFont(1);
 		if((i+1) <= Highscore.count()){
 			Serial.println(Highscore.get(i).score);
-			baseSprite->printf("%.3s  %4d",Highscore.get(i).name, Highscore.get(i).score);
+			baseSprite->printf("  %.3s  %4d",Highscore.get(i).name, Highscore.get(i).score);
 		}
 	}
 
@@ -84,10 +83,10 @@ void CastleJump::ShowHighscoreState::loop(uint time){
 }
 
 void CastleJump::ShowHighscoreState::drawScrollText(){
-	baseSprite->setCursor(14, 2);
+	baseSprite->setCursor(14, 4);
 	baseSprite->setTextSize(1);
 	baseSprite->setTextFont(1);
-	baseSprite->print("Press UP to erase");
+	baseSprite->print("   Press UP to erase");
 }
 
 
