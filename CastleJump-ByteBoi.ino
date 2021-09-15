55179c31e202ae6bc5ad4c439f15dc66fb9f8dd7
hrvojekolar <hrvojekolar1998@gmail.com>
2021-09-15 08:50:25 +0200

    add new ino file

---
+++ CastleJump-ByteBoi.ino
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include <CircuitOS.h>
+#include <Input/I2cExpander.h>
+#include <Input/InputI2C.h>
+#include <Loop/LoopManager.h>
+#include "src/CastleJump.h"
+#include "src/GameState.h"
+#include <ByteBoi.h>
+#include <SPIFFS.h>
+
+CastleJump::CastleJump* castleJump;
+Sprite *baseSprite;
+
+void setup(){
+	Serial.begin(115200);
+	ByteBoi.begin();
+	ByteBoi.getDisplay()->commit();
+	LoopManager::addListener(Input::getInstance());
+	castleJump=new CastleJump::CastleJump(ByteBoi.getDisplay());
+	LoopManager::addListener(castleJump);
+	castleJump->unpack();
+	castleJump->start();
+}
+
+void loop(){
+	LoopManager::loop();
+
+}
